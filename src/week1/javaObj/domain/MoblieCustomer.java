33c619af923e7e5846edf774a2e25abd6ddfb380
Kyunghun Kim <kkhkr98@gmail.com>
2023-07-20 19:36:05 +0900

    fix : fix getinfo method
    add week1Diagram

--- src/week1/javaObj/domain/MoblieCustomer.java
+++ src/week1/javaObj/domain/MoblieCustomer.java
@@ -16,10 +16,11 @@ public class MoblieCustomer extends Customer{
 
     @Override
     public void getUserInfo() {
-        System.out.println("접속 경로: " + getConnection());
-        System.out.println("이름: " + getName());
-        System.out.println("등급: " + getGrade());
-        System.out.println("결제 방식: " + getDiscountPolicy());
+        System.out.println("접속 경로: " + getConnection()
+                        + "\n이름: " + getName()
+                        + "\n등급: " + getGrade()
+                        + "\n결제 방식: " + getDiscountPolicy()
+                );
         if (getDiscountPolicy() instanceof BonusDiscountPolicy) {
             System.out.println("보너스: " + getBonusPoint());
         }
